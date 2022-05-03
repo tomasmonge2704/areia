3959de96e1cc5b6015c6db09b65c477c45a0640a
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2022-05-03 17:28:26 -0300

    fix mapa

--- src/components/NabVar/mapa.js
+++ src/components/NabVar/mapa.js
@@ -1,23 +1,23 @@
-import imagenMapa from "../../SELECCION DE FOTOS/Untitled.png"
+import imagenMapa from "../../SELECCION DE FOTOS/mapa.png"
 import imagenMapa2 from "../../SELECCION DE FOTOS/mapa2.png"
 
 export default function Mapa() {
     return (
         <div style={{ backgroundColor: "#1c1c1c" }}>
-            <div id="mapa1">
+            <div>
 
-                <img src={imagenMapa} style={{ width: "100%", height: "100%" }} />
+            <img src={imagenMapa} id="mapa" />
 
             </div>
             <div id="comoLlegar">
                 <div id="textMapa">
                 <a href="https://linktr.ee/estoesareia">
-                <button data-aos="fade-up" id="bottone1"><strong>COMO LLEGAR</strong>
+                <button id="bottone1"><strong>COMO LLEGAR</strong>
                 </button>
                 </a>
                 </div>
                 <div id="mapa2">
-                    <img src={imagenMapa2} style={{ width: "100%", height: "100%" }} />
+                    <img src={imagenMapa2} style={{ width: "100%", height: "100%",padding:"5%",filter:"drop-shadow(2px 4px 6px black)" }} />
                 </div>
 
 
