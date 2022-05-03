eda870368cd8ca6cdaa67b304cab1943ad76b068
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2022-05-03 16:35:14 -0300

    responsive fix

--- src/components/NabVar/mapa.js
+++ src/components/NabVar/mapa.js
@@ -4,16 +4,19 @@ import imagenMapa2 from "../../SELECCION DE FOTOS/mapa2.png"
 export default function Mapa() {
     return (
         <div style={{ backgroundColor: "#1c1c1c" }}>
-            <div style={{ height: "40rem" }}>
+            <div id="mapa1">
 
                 <img src={imagenMapa} style={{ width: "100%", height: "100%" }} />
 
             </div>
-            <div style={{ height: "40rem",display:"flex",justifyContent:"space-evenly", alignItems:"center" }}>
-                <div style={{marginBottom:"15rem"}}>
-                    <h1>CÓMO LLEGAR</h1>
+            <div id="comoLlegar">
+                <div id="textMapa">
+                <a href="https://linktr.ee/estoesareia">
+                <button data-aos="fade-up" id="bottone1"><strong>COMO LLEGAR</strong>
+                </button>
+                </a>
                 </div>
-                <div style={{width:"50%"}}>
+                <div id="mapa2">
                     <img src={imagenMapa2} style={{ width: "100%", height: "100%" }} />
                 </div>
 
