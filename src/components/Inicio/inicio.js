94aab4c68ef85153813c5053be85def983f74af5
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2022-08-26 01:57:49 -0300

    fix

--- src/components/Inicio/inicio.js
+++ src/components/Inicio/inicio.js
@@ -6,7 +6,7 @@ import './Inicio.css'
 export default function Inicio() {
   return (
     <div>
-      <img className="imgInicio" src={Imagen} style={{ width: "100%",maxHeight:"60vh",objectFit:"cover" }} />
+      <img className="imgInicio" src={Imagen} style={{ width: "100%",maxHeight:"70vh",objectFit:"cover" }} />
       <div
         style={{
           backgroundColor: "white",
@@ -18,18 +18,18 @@ export default function Inicio() {
         }}
         className='divInicio'
       >
-        <p style={{ color: "black", fontSize: "40px" }}>AREIA APP</p>
+        <p style={{ color: "black", fontSize: "30px" }}>AREIA APP</p>
         <div className="storeButton">
-          <a href="https://apps.apple.com/ar/app/areia/id1437104821" style={{ marginRight: "10px" }} target="_blank">
+          <a href="https://apps.apple.com/ar/app/areia/id1437104821" style={{ marginRight: "10px"}} target="_blank">
           <img src={appStore}/>
           </a>
-          <a href="https://play.google.com/store/apps/details?id=com.fansbury.AREIA&hl=es-419" style={{ marginLeft: "10px" }} target="_blank">
-          <img src={playStore}  />
+          <a href="https://play.google.com/store/apps/details?id=com.fansbury.AREIA&hl=es-419" style={{ marginLeft: "10px"}} target="_blank">
+          <img src={playStore}/>
           </a>
         </div>
         <p
           className="textInicio"
-          style={{ color: "black", fontSize: "20px", marginTop: "30px" }}
+          style={{ color: "black", fontSize: "16px", marginTop: "30px" }}
         >
           POSICIONES, ESTADÍSTICAS, CANCHAS, NOTICIAS Y MÁS
           <br />
